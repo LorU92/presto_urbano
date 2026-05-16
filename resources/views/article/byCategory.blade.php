9d56d08cac73500ff6da0e6565d9e5569e1b748f
Lorenzo Urbano <lorenzourbano92@gmail.com>
2026-05-16 21:16:03 +0200

    user story 02 - aggiornata e completata

--- resources/views/article/byCategory.blade.php
+++ resources/views/article/byCategory.blade.php
@@ -11,25 +11,23 @@
                     <div class="row">
                         <x-card :article="$article" />
                     </div>
-            @empty
+                    @empty
                     <div class="row">
                         <div class="col-12">
                         </div>
+                    </div>       
+                    <div class="d-flex justify-content-center align-items-center flex-column">
+                        <div>
+                            <h3 class="text-center sottotitleevento">ANCORA NESSUN ARTICOLO? </br> Non ti preoccupare arriveranno presto!</h3>
+                        </div>
+                        @auth
+                        <button class="button-news my-3 p-2">
+                            <a href="{{route('create.article')}}"><h4>CREA ARTICOLO<h4></a>
+                        </button>
+                        @endauth
                     </div>
-                </div>
-            @endforelse
-        
-        <div class="d-flex justify-content-center align-items-center flex-column">
-            <div>
-                <h3 class="text-center sottotitleevento">ANCORA NESSUN ARTICOLO? </br> Non ti preoccupare arriveranno presto!</h3>
-            </div>
-            @auth
-                <button class="button-news my-3 p-2">
-                    <a href="{{route('create.article')}}"><h4>CREA ARTICOLO<h4></a>
-                </button>
-
-            @endauth
-        </div>
+                    @endforelse
+                </div>       
         <div class="d-flex justify-content-center">
             <div>
                 {{-- metodo per generare le pagine da selezionare  --}}
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -9,7 +9,11 @@
     <h3 class="sottotitleevento">{{$article->price}} € </h3>
     <button class="button-news">
         <a href="{{route('show.article', compact('article'))}}"><h4>LEGGI TUTTO<h4></a>
-        </button>
+    </button>
+    <button class="button-news">
+        {{-- rotta parametrica byCategory passando come parametro la cetegoria collegato all'articolo --}}
+        <a href="{{route('byCategory', ['category' => $article->category])}}"><h4>{{$article->category?->name}}</h4></a>
+    </button>
     </div>
     <div class="col-12 col-md-5 eventotext-custom">{{Str::limit($article->description, 300)}}</div>
     <div class="linea"></div>
